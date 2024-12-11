4a167fe4ff03c77f4ab1bc8986b05b032035149f
ruffinengobrunoo <167806007+ruffinengobrunoo@users.noreply.github.com>
2024-12-11 18:01:37 -0300

    Update conexion.js

--- db/conexion.js
+++ db/conexion.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql2');
 
-// const conexion = mysql.createConnection({
-//     host: process.env.MYSQL_ADDON_HOST || "localhost",
-//     user: process.env.MYSQL_ADDON_USER || "root",
-//     password: process.env.MYSQL_ADDON_PASSWORD || "",
-//     database: process.env.MYSQL_ADDON_DB || "tienda",
-//     connectionLimit: 5
-// });
-
 const conexion = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "tienda",
+    host: process.env.MYSQL_ADDON_HOST || "localhost",
+    user: process.env.MYSQL_ADDON_USER || "root",
+    password: process.env.MYSQL_ADDON_PASSWORD || "",
+    database: process.env.MYSQL_ADDON_DB || "tienda",
     connectionLimit: 5
 });
+
+// const conexion = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "tienda",
+//     connectionLimit: 5
+// });
 
 conexion.connect((err)=>{
     if(err){
